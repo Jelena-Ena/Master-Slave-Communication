47c91fc08a11fedf0dfb6a25c2b4f5233e7537db
Jelena-Ena <33864251+Jelena-Ena@users.noreply.github.com>
2017-11-23 12:01:35 +0100

    Create Slave.ino

---
+++ Slave.ino
@@ -0,0 +1,317 @@
+#include <Wire.h>                       //library for communication with I2C devices
+#include <LiquidCrystal_I2C.h>          // library for LCD with I2C communication
+
+// ?
+#if defined(ARDUINO) && ARDUINO >= 100
+#define printByte(args)  write(args);
+#else
+#define printByte(args)  print(args,BYTE);
+#endif
+
+byte x,y,z;
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  while (1 < Wire.available()) {
+    // reading digits from master
+    x = Wire.read();
+    y = Wire.read();
+    z = Wire.read();
+    delay(100);
+  }
+}
+
+
+//      Segments for characters        //
+byte bar1[8] = 
+{
+        B11100,
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11110,
+        B11100
+};
+byte bar2[8] =
+{
+        B00111,
+        B01111,
+        B01111,
+        B01111,
+        B01111,
+        B01111,
+        B01111,
+        B00111
+};
+byte bar3[8] =
+{
+        B11111,
+        B11111,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B11111,
+        B11111
+};
+byte bar4[8] =
+{
+        B11110,
+        B11100,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B11000,
+        B11100
+};
+byte bar5[8] =
+{
+        B01111,
+        B00111,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00011,
+        B00111
+};
+byte bar6[8] =
+{
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B11111,
+        B11111
+};
+byte bar7[8] =
+{
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00111,
+        B01111
+};
+byte bar8[8] =
+{
+        B11111,
+        B11111,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000,
+        B00000
+};
+
+LiquidCrystal_I2C lcd(0x3e,16,2);        // seting the LCD address for a 16 chars and 2 line display
+ 
+/*               Functions for building numbers                */
+/* Every number has 6 segments, 3 in first row and 3 in second */ 
+void custom0(int col)
+{ 
+  lcd.setCursor(col, 0); 
+  lcd.write(2);  
+  lcd.write(8); 
+  lcd.write(1);
+  lcd.setCursor(col, 1); 
+  lcd.write(2);  
+  lcd.write(6);  
+  lcd.write(1);
+}
+
+void custom1(int col)
+{
+  
+  lcd.setCursor(col,0);
+  lcd.write(32);
+  lcd.write(32);
+  lcd.write(1);
+  lcd.setCursor(col,1);
+  lcd.write(32);
+  lcd.write(32);
+  lcd.write(1);
+}
+
+void custom2(int col)
+{
+  lcd.setCursor(col,0);
+  lcd.write(5);
+  lcd.write(3);
+  lcd.write(1);
+  lcd.setCursor(col, 1);
+  lcd.write(2);
+  lcd.write(6);
+  lcd.write(6);
+}
+
+void custom3(int col)
+{
+  lcd.setCursor(col,0);
+  lcd.write(5);
+  lcd.write(3);
+  lcd.write(1);
+  lcd.setCursor(col, 1);
+  lcd.write(7);
+  lcd.write(6);
+  lcd.write(1); 
+}
+
+void custom4(int col)
+{
+  lcd.setCursor(col,0);
+  lcd.write(2);
+  lcd.write(6);
+  lcd.write(1);
+  lcd.setCursor(col, 1);
+  lcd.write(32);
+  lcd.write(32);
+  lcd.write(1);
+}
+
+void custom5(int col)
+{
+  lcd.setCursor(col,0);
+  lcd.write(2);
+  lcd.write(3);
+  lcd.write(4);
+  lcd.setCursor(col, 1);
+  lcd.write(7);
+  lcd.write(6);
+  lcd.write(1);
+}
+
+void custom6(int col)
+{
+  lcd.setCursor(col,0);
+  lcd.write(2);
+  lcd.write(3);
+  lcd.write(4);
+  lcd.setCursor(col, 1);
+  lcd.write(2);
+  lcd.write(6);
+  lcd.write(1);
+}
+
+void custom7(int col)
+{
+  lcd.setCursor(col,0);
+  lcd.write(2);
+  lcd.write(8);
+  lcd.write(1);
+  lcd.setCursor(col, 1);
+  lcd.write(32);
+  lcd.write(32);
+  lcd.write(1);
+}
+
+void custom8(int col)
+{
+  lcd.setCursor(col, 0); 
+  lcd.write(2);  
+  lcd.write(3); 
+  lcd.write(1);
+  lcd.setCursor(col, 1); 
+  lcd.write(2);  
+  lcd.write(6);  
+  lcd.write(1);
+}
+
+void custom9(int col)
+{
+  lcd.setCursor(col, 0); 
+  lcd.write(2);  
+  lcd.write(3); 
+  lcd.write(1);
+  lcd.setCursor(col, 1); 
+  lcd.write(7);  
+  lcd.write(6);  
+  lcd.write(1);
+}
+
+/*     Function that displays number according to its value and place on LCD    */
+void printNumber(int value, int col) {
+  if (value == 0) {
+    custom0(col);
+  }
+  if (value == 1) {
+    custom1(col);
+  }
+  if (value == 2) {
+    custom2(col);
+  }
+  if (value == 3) {
+    custom3(col);
+  }
+  if (value == 4) {
+    custom4(col);
+  }
+  if (value == 5) {
+    custom5(col);
+  }
+  if (value == 6) {
+    custom6(col);
+  }
+  if (value == 7) {
+    custom7(col);
+  }
+  if (value == 8) {
+    custom8(col);
+  }
+  if (value == 9) {
+    custom9(col);
+  }      
+}  
+
+void setup()
+{
+  lcd.init();                      
+  lcd.backlight();
+
+  // creating custom characters (max 8) 
+  lcd.createChar(1,bar1);
+  lcd.createChar(2,bar2);
+  lcd.createChar(3,bar3);
+  lcd.createChar(4,bar4);
+  lcd.createChar(5,bar5);
+  lcd.createChar(6,bar6);
+  lcd.createChar(7,bar7);
+  lcd.createChar(8,bar8);
+
+  // letter E on LCD
+  lcd.setCursor(0, 0); 
+  lcd.write(2);  
+  lcd.write(3); 
+  lcd.write(3);
+  lcd.setCursor(0, 1); 
+  lcd.write(2);  
+  lcd.write(6);  
+  lcd.write(6);
+
+  // letter P
+  lcd.setCursor(3, 0); 
+  lcd.write(2);  
+  lcd.write(3); 
+  lcd.write(1);
+  lcd.setCursor(3, 1); 
+  lcd.write(2);
+
+  // displaying number received from master
+  while(1){
+   printNumber(x, 7);
+   printNumber(y, 10);
+   printNumber(z, 13);
+   delay(1000);
+  }
+}
+
+
